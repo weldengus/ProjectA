d2a685ab0eb88795ca160ee4a90e2b9490ca4e30
Ezra ITC <Ezra ITC@Ezra.lan>
2024-01-03 21:03:13 -0500

    INITIAL COMMIT

---
+++ src/c/DemoC.java
@@ -0,0 +1,9 @@
+package c;
+
+public class DemoC {
+public static void main(String[] args) {
+	System.out.println("C");
+	System.out.println("batch 11");
+	System.out.println(" Batch 11 Git lecture");
+}
+}
